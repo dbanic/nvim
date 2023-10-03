6fc7b53fad3c0eb4f34252bb616b5372f92ac6f2
Damir Banić <damir.banic@gmail.com>
2023-10-03 21:17:24 +0200

    Migrate indent blankline plugin to version 3

--- lua/packages/indent-blankline.lua
+++ lua/packages/indent-blankline.lua
@@ -1,8 +1,2 @@
-vim.opt.list = false
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
-
-require("indent_blankline").setup {
-    show_end_of_line = true,
-    space_char_blankline = " ",
+require("ibl").setup {
 }
